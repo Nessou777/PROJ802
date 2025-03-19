1d4e637bb7805c2030c049dbb4e3323fb96460a3
INES VEIGNEAU <ines.veigneau@gmail.com>
2025-03-19 14:05:03 +0100

    test : ajout affichage

---
+++ Projet/Projet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Projet
+//
+//  Created by Inès Veigneau on 19/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
